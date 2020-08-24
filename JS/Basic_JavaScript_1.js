58cc31f292477153b3b2e9165da138b51f579335
Hunter-503 <69371708+Hunter-503@users.noreply.github.com>
2020-08-24 16:11:15 -0700

    JavaScript Assinment
    1st javascript assinment

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,13 @@
+var B = "Concatenated", A = " String", F = "Line", K = "Brush",
+    H = "penny", Dad = "LOL"
+
+var  A = A.fontcolor("red");
+var B = B.fontcolor("blue");
+var F = F.fontcolor("pink");
+var K = K.fontcolor("orange");
+var H = H.fontcolor("yellow");
+var Dad = Dad.fontcolor("rainbow");
+
+document.write(Dad,B, A, F, K, H) 
+
+document.write(10 + 10); 
---
+++ JS/external.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");  //This code didnt work for me
+    document.getElementById("Green_Text").innerHTML = result;
+}
