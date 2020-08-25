1d1c4d7c501321d7866505f286421523f05e1812
Hunter-503 <69371708+Hunter-503@users.noreply.github.com>
2020-08-25 13:32:23 -0700

    JavaScript 2
    2nd assignment!

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,10 @@
+function changeColor(newColor) {
+    var elem = document.getElementById('cap');
+    elem.style.color = newColor;
+  }
+
+  function myFunction() {
+    var paragraph = "JavaScript doesnt make sense";
+    paragraph += " I hope I don't fail this course!";
+    document.getElementById("isuck").innerHTML = paragraph;
+}
